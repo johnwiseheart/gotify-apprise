cd2bcbfa809d3ab56a75c4e5aac9a0504eeeccfa
John Wiseheart <johnwiseheart@gmail.com>
2024-07-01 08:17:05 +0000

    Extra debug + fix

--- proxy.py
+++ proxy.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 gotify_host = os.environ['GOTIFY_HOST']
 gotify_token = os.environ['GOTIFY_TOKEN']
-apprise_config = os.environ['APPRISE_CONFIG'] if 'APPRISE_CONFIG' in os.environ else '/config/apprise.conf'
+apprise_config = os.environ['APPRISE_CONFIG'] if 'APPRISE_CONFIG' in os.environ else '/config/apprise.yaml'
 
 config = AppriseConfig()
 config.add(f"file://{apprise_config}")
@@ -18,10 +18,11 @@ apprise = Apprise()
 apprise.add(config)
 
 def on_message(ws, message):
+    print("RECV:", message)
     msg = json.loads(message)
     apprise.notify(
-        body=msg['title'],
-        title=msg['message'],
+        title=msg['title'],
+        body=msg['message'],
     )
 
 def on_error(ws, error):
@@ -34,6 +35,7 @@ def on_open(ws):
     print("Opened Gotify websocket connection")
 
 if __name__ == "__main__":
+    print("Starting Gotify Apprise")
     wsapp = websocket.WebSocketApp("ws://" + str(gotify_host) + "/stream", header={"X-Gotify-Key": str(gotify_token)},
                                    on_open=on_open,
                                    on_message=on_message,
